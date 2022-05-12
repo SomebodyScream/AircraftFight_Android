76f63c63f44ef312d389d8c83e1a7ac75940d451
Aucki6144 <63536053+aucki6144@users.noreply.github.com>
2022-05-12 16:48:44 +0800

    A1
    Finished ChoiceFragment and the basic of MainActivity, added some picture sources.The UI of ChoiceFragment need to be updated later.

---
+++ app/src/main/java/com/example/aircraftfight_android/activity/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.aircraftfight_android.activity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.aircraftfight_android.R;
+import com.example.aircraftfight_android.fragment.ChoiceFragment;
+import com.example.aircraftfight_android.fragment.ConnectFragment;
+
+import java.lang.reflect.Field;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+        Fragment fragment = new ChoiceFragment();
+        initialFragment(fragment);
+
+    }
+
+    public void replaceFragmentMulti(){
+        replaceFragment(new ConnectFragment());
+    }
+
+    public void startSettingActivity(){
+        Intent intent = new Intent(this, SettingActivity.class);
+        startActivity(intent);
+    }
+
+    private void initialFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.main_activity_fragment_content,fragment);
+        transaction.commit();
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.main_activity_fragment_content,fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+}
---
+++ app/src/main/java/com/example/aircraftfight_android/fragment/ChoiceFragment.java
@@ -0,0 +1,60 @@
+package com.example.aircraftfight_android.fragment;
+
+import android.graphics.Color;
+import android.media.Image;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.example.aircraftfight_android.R;
+import com.example.aircraftfight_android.activity.MainActivity;
+
+
+public class ChoiceFragment extends Fragment {
+
+    public ChoiceFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_choice, container, false);
+        MainActivity activity = (MainActivity) getActivity();
+
+        // View instance initialize
+        ImageButton buttonMultiplayer = view.findViewById(R.id.button_multiplayer);
+        ImageButton buttonSinglePlayer = view.findViewById(R.id.button_singleplayer);
+        ImageButton buttonSetting = view.findViewById(R.id.button_setting);
+        ImageButton buttonStore = view.findViewById(R.id.button_store);
+
+
+
+        // Listeners set
+        buttonMultiplayer.setOnClickListener(v -> {
+            if (activity!=null){
+                activity.replaceFragmentMulti();
+            }
+        });
+
+        buttonSetting.setOnClickListener(v -> {
+            if (activity!=null){
+                activity.startSettingActivity();
+            }
+        });
+
+        return view;
+    }
+}
